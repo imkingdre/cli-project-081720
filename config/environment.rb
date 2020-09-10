99e2071fa11d22f6673eeb4f0c015228a3ed6bf2
andreking1031 <andreking1031@gmail.com>
2020-09-10 15:25:28 -0400

    add code

--- config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'pry'
-require 'nokogiri'
-require 'open-uri'
-
-require_relative '../lib/the-office-directory/cli'
-require_relative '../lib/the-office-directory/cast'
-require_relative '../lib/the-office-directory/cast-scraper'
-require_relative '../lib/the-office-directory/locations'
-require_relative '../lib/the-office-directory/locations-scraper'
--- lib/the_office_directory.rb
+++
@@ -1,6 +0,0 @@
-module TheOfficeDirectory
-    #class Error < StandardError; end
-
-end
-
-require_relative '../config/environment'
